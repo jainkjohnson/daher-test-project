0838aaad051187d0051cf391078f966dcab76e2f
jain <jain@qburst.com>
2020-01-11 12:46:36 +0530

    Initial set for react-native-web

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+
+import * as React from "react";
+import Home from "./container/home";
+
+const App: React.FC = () => <Home />
+
+export default App;
---
+++ src/container/home.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Text, View } from "react-native-web";
+
+const Home: React.FC = () => {
+  return (
+    <View>
+      <Text>home page</Text>
+    </View>
+  );
+}
+
+export default Home
