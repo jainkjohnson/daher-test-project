9777c6b09d05e750f1c2bb12611b8233ba185d9c
jainkjohnson <jainkjohnson@gmail.com>
2020-01-13 13:35:22 +0530

    Add config file and create generateJson function to create dynamic data to tree
    - Split styles to separate file
    - Add link to source code

---
+++ src/component/tree/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    icon: {
+      width: 10,
+      height: 10,
+      margin: 10,
+    },
+    childrenNode: { paddingLeft: 30 },
+  });
+
+  export default styles;
---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+const config = {
+    region: 1,
+    site: 2,
+    activity: 2,
+    workspace: 2
+  }
+  
+  export default config;
